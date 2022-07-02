b3326013df0155c6eaed1cd9d91c4be0ff3f6881
guswn2521 <cwjcsk@naver.com>
2022-07-02 21:55:21 +0900

    Create graph.py
    graph 설정 모듈

---
+++ modules/graph.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+
+def graph_style():
+    # 그래프 스타일 설정
+    plt.rcParams['xtick.color'] = 'white'
+    plt.rcParams['xtick.labelsize'] = 15
+    plt.rcParams['ytick.color'] = 'white'
+    plt.rcParams['ytick.labelsize'] = 15
+    plt.rcParams['axes.labelcolor'] = 'white'
+    plt.rcParams['axes.labelsize'] = 17
+    plt.rcParams['axes.titlecolor'] = 'white'
+    plt.rcParams['axes.titlesize'] = 20
+    plt.rcParams['figure.figsize'] = (10, 6)
